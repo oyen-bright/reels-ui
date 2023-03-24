c10042497777c2a84eab1140d4ebb9fd9100bcd3
oyen-bright <oyenbrihight@gmail.com>
2023-03-24 19:06:01 +0100

    reels widget created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,6 +14,13 @@ import 'cubit/opacity_cubit.dart';
 void main() {
   runApp(const MyApp());
 }
+
+List<String> reelsUrl = [
+  "https://assets.mixkit.co/videos/preview/mixkit-man-under-colored-lights-1241-large.mp4",
+  "https://assets.mixkit.co/videos/preview/mixkit-silhouette-of-urban-dancer-in-smoke-33898-large.mp4",
+  "https://assets.mixkit.co/videos/preview/mixkit-cheerful-man-moves-forward-dancing-in-the-middle-of-nature-32746-large.mp4",
+  "https://assets.mixkit.co/videos/preview/mixkit-man-runs-past-ground-level-shot-32809-large.mp4"
+];
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -46,7 +54,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   late final ScrollController scrollController;
   late final PageController pageController;
-  late VideoPlayerController _controller;
 
   @override
   void initState() {
@@ -56,18 +63,7 @@ class _HomeState extends State<Home> {
 
     super.initState();
 
-    _controller = VideoPlayerController.network(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
-      ..initialize().then((_) {
-        _controller.setLooping(true);
-        setState(() {
-          _controller.play();
-        });
-      });
-
     pageController.addListener(() {
-      print(pageController.offset);
-
       if (pageController.page!.round() != currentPage) {
         currentPage = pageController.page!.round();
         Timer(const Duration(milliseconds: 300), () {
@@ -92,96 +88,14 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Colors.transparent,
       child: Stack(
         children: [
           PageView(
             controller: pageController,
             scrollDirection: Axis.vertical,
-            children: [
-              Container(
-                child: _controller.value.isInitialized
-                    ? Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: [
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height,
-                            child: AspectRatio(
-                              aspectRatio: 16 / 9,
-                              child: VideoPlayer(_controller),
-                            ),
-                          ),
-                          Container(
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Container(
-                                    height: 200,
-                                  ),
-                                ),
-                                BlocBuilder<OpacityCubit, double>(
-                                  builder: (context, state) {
-                                    return Opacity(
-                                      opacity: state,
-                                      child: SizedBox(
-                                        height: 350,
-                                        width: 50,
-                                        child: Column(
-                                          children: [
-                                            const SideBarWidget(
-                                              icondata: FontAwesomeIcons.heart,
-                                              text: "62.3k",
-                                            ),
-                                            20.height,
-                                            const SideBarWidget(
-                                              icondata:
-                                                  FontAwesomeIcons.comment,
-                                              text: "804",
-                                            ),
-                                            20.height,
-                                            const SideBarWidget(
-                                              icondata:
-                                                  FontAwesomeIcons.paperPlane,
-                                              text: "",
-                                            ),
-                                            15.height,
-                                            const SideBarWidget(
-                                              icondata: Icons.more_horiz,
-                                              text: "",
-                                            ),
-                                            Container(
-                                              height: 40,
-                                              width: 40,
-                                              padding: const EdgeInsets.all(3),
-                                              decoration: BoxDecoration(
-                                                  color: Colors.white,
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          10)),
-                                              child: Image.network(
-                                                  "https://avatars.githubusercontent.com/u/35768100?s=40&v=4"),
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      )
-                    : Container(
-                        color: Colors.black,
-                        child: const Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      ),
-              ),
-              Container(
-                color: Colors.pink,
-              )
-            ],
+            children: List.generate(reelsUrl.length,
+                (index) => ReelWidget(videoUrl: reelsUrl[index])),
           ),
           SafeArea(
             child: Container(
@@ -219,6 +133,237 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+    );
+  }
+}
+
+class ReelWidget extends StatefulWidget {
+  const ReelWidget({
+    super.key,
+    required this.videoUrl,
+  });
+
+  final String videoUrl;
+
+  @override
+  State<ReelWidget> createState() => _ReelWidgetState();
+}
+
+class _ReelWidgetState extends State<ReelWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.network(widget.videoUrl)
+      ..initialize().then((_) {
+        _controller.setLooping(true);
+        setState(() {
+          _controller.play();
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: _controller.value.isInitialized
+          ? Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: VideoPlayer(_controller),
+                  ),
+                ),
+                SafeArea(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 200,
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Row(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(100),
+                                    child: Container(
+                                      height: 40,
+                                      width: 40,
+                                      padding: const EdgeInsets.all(3),
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(100)),
+                                      child: Image.network(
+                                          "https://avatars.githubusercontent.com/u/35768100?s=40&v=4"),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 10),
+                                    child: Text("swimming_pictures",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleMedium!
+                                            .copyWith(
+                                                fontWeight: FontWeight.w500,
+                                                color: Colors.white)),
+                                  ),
+                                  Container(
+                                    height: 25,
+                                    alignment: Alignment.center,
+                                    width: 65,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(
+                                          width: 0.5,
+                                          color: Colors.white,
+                                        )),
+                                    child: Text(
+                                      "Follow",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleSmall!
+                                          .copyWith(
+                                              fontWeight: FontWeight.w500,
+                                              color: Colors.white),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              10.height,
+                              SizedBox(
+                                width: double.infinity,
+                                child: AutoSizeText(
+                                  "I know it looks scary but because of there things and all ",
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleLarge!
+                                      .copyWith(color: Colors.white),
+                                ),
+                              ),
+                              10.height,
+                              Row(
+                                children: [
+                                  const FaIcon(
+                                    FontAwesomeIcons.music,
+                                    color: Colors.white,
+                                    size: 15,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 10),
+                                    child: Text("swimming_pictures",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleSmall!
+                                            .copyWith(color: Colors.white)),
+                                  ),
+                                  Container(
+                                    height: 2,
+                                    width: 2,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius:
+                                            BorderRadius.circular(100)),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 10),
+                                    child: Text("Original audio",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleSmall!
+                                            .copyWith(color: Colors.white)),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      BlocBuilder<OpacityCubit, double>(
+                        builder: (context, state) {
+                          return Opacity(
+                            opacity: state,
+                            child: SizedBox(
+                              height: 350,
+                              width: 50,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  const SideBarWidget(
+                                    icondata: FontAwesomeIcons.heart,
+                                    text: "62.3k",
+                                  ),
+                                  20.height,
+                                  const SideBarWidget(
+                                    icondata: FontAwesomeIcons.comment,
+                                    text: "804",
+                                  ),
+                                  20.height,
+                                  const SideBarWidget(
+                                    icondata: FontAwesomeIcons.paperPlane,
+                                    text: "",
+                                  ),
+                                  15.height,
+                                  const SideBarWidget(
+                                    icondata: Icons.more_horiz,
+                                    text: "",
+                                  ),
+                                  Container(
+                                    height: 40,
+                                    width: 40,
+                                    padding: const EdgeInsets.all(3),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child: Image.network(
+                                        "https://avatars.githubusercontent.com/u/35768100?s=40&v=4"),
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Colors.transparent, Colors.black26])),
+                )
+              ],
+            )
+          : Container(
+              color: Colors.black,
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
     );
   }
 }
